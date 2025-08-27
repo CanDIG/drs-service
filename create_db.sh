8d2e3cfae72eb1dcb26540ea525e0af7db1edeb6
Daisie Huang <daisieh@gmail.com>
2025-08-27 10:20:05 -0700

    migrate tables from genomic

--- create_db.sh
+++ create_db.sh
@@ -17,6 +17,23 @@ done
 echo "database is at $db"
 pwd
 
+# migrate tables from genomic db, if existing
+psql --quiet -h "$db" -U $PGUSER -d genomic -c "SELECT * from drs_object limit 1"
+if [[ $? -eq 0 ]]; then
+    echo "migrating tables..."
+    createdb -h "$db" -U $PGUSER drs
+    pg_dump -h "$db" -U $PGUSER -t program genomic | psql -h "$db" -U $PGUSER -d drs
+    pg_dump -h "$db" -U $PGUSER -t drs_object genomic | psql -h "$db" -U $PGUSER -d drs
+    pg_dump -h "$db" -U $PGUSER -t access_method genomic | psql -h "$db" -U $PGUSER -d drs
+    pg_dump -h "$db" -U $PGUSER -t content_object genomic | psql -h "$db" -U $PGUSER -d drs
+    psql --quiet -h "$db" -U $PGUSER -d genomic -c "DROP TABLE IF EXISTS content_object"
+    psql --quiet -h "$db" -U $PGUSER -d genomic -c "DROP TABLE IF EXISTS access_method"
+    psql --quiet -h "$db" -U $PGUSER -d genomic -c "ALTER TABLE variantfile DROP CONSTRAINT variantfile_drs_object_id_fkey"
+    psql --quiet -h "$db" -U $PGUSER -d genomic -c "DROP TABLE IF EXISTS drs_object"
+    psql --quiet -h "$db" -U $PGUSER -d genomic -c "DROP TABLE IF EXISTS program"
+    echo "...done"
+fi
+
 psql --quiet -h "$db" -U $PGUSER -d drs -c "SELECT * from program limit 1"
 if [[ $? -ne 0 ]]; then
     echo "initializing database..."
@@ -24,4 +41,3 @@ if [[ $? -ne 0 ]]; then
     psql --quiet -h "$db" -U $PGUSER -a -d drs -f data/drs.sql >>setup_out.txt
     echo "...done"
 fi
-
